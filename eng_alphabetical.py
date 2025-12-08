31cd4f40725ed725d981b83224514e64df288b01
Mehmet Burak Menteşe <burakmentese16@gmail.com>
2025-12-08 23:14:38 +0300

    Create eng_alphabetical.py
    Code has been added as English version.
    Koda İngilizce sürüm eklendi.

---
+++ eng_alphabetical.py
@@ -0,0 +1,7 @@
+while True:
+    def alphabetical_sort():
+        words = input("Enter the words separated by spaces: ").split()
+        words.sort()
+        print("Alphabetical order: ", words)
+
+    alphabetical_sort()
